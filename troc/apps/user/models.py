9ad2c99d9a290684e86b682341bec25af2e439b8
Windfarer <windfarer@gmail.com>
2019-09-28 15:08:15 +0800

    wechat login jwt

--- troc/apps/user/models.py
+++ troc/apps/user/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class WxAppUser(AbstractUser):
     openid = models.CharField(max_length=200)
-    unionid = models.CharField(max_length=200)
+    unionid = models.CharField(max_length=200, null=True)
     session_key = models.CharField(max_length=200)
